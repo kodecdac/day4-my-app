14dec7d8c7a74cd4b878e96d19f6ca6f358132ae
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-03-03 11:25:21 +0530

    working with statful objec.t

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import { useState } from "react";
 
 export default function App() {
-  let [username, setUsername] = useState("Rohit");
-  let [follower, setFollower] = useState(10);
+  // let user = { usenrame: "rohit", follower: 100 };
+  let [user, setUser] = useState({ username: "rohit", follower: 100 });
 
-  let handleFollower = () => {
-    let newFollowercount = follower + 10;
-    // UPdate the UI with new Value.
-    setFollower(newFollowercount);
+  const updateFollower = () => {
+    let newFollower = user.follower + 10;
+    setUser({ ...user, follower: newFollower });
   };
 
-  let updateUsername = () => {
-    let newUsername = username + " Sharmmaa";
-    setUsername(newUsername);
+  const updateUsername = () => {
+    let newUsername = user.username + " sharma";
+    setUser({ ...user, username: newUsername });
   };
 
   return (
     <div>
       <h1>
-        {username} has {follower} followers
+        {user.username} has {user.follower} follower
       </h1>
 
-      <input type="button" value="Update FOLLOWERS" onClick={handleFollower} />
-      <input type="button" value="UPDATE USERNAME" onClick={updateUsername} />
+      <input type="button" value="Update Follower" onClick={updateFollower} />
+      <input type="button" value="Update Username" onClick={updateUsername} />
     </div>
   );
 }
