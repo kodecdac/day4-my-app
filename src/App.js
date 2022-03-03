e0151ff5a615fe82b0e39e56c2e2a126080891dd
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-03-03 10:41:14 +0530

    counter app, but ui is not rendering.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 function App() {
-  let counter = 10;
-  let title = "State Management ###";
+  let likeCounter = 0;
 
-  // USED FOR EVENT HANDLING
-  let clickMe = () => {
-    console.log("Function is called!!");
-    alert();
+  let likeMe = () => {
+    likeCounter++;
+    console.log(likeCounter);
   };
 
   return (
     <div>
-      <h1>{title}</h1>
-      <h1>{counter}</h1>
+      <h1>{likeCounter}</h1>
 
-      <input type="button" value="Click Me" onClick={clickMe} />
+      <input type="button" value="Like Me" onClick={likeMe} />
     </div>
   );
 }
