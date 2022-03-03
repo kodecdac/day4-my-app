663f18dfa4d1bf7fb581699edd88db806d213173
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-03-03 11:06:40 +0530

    state-management react, with es6 feature.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import { useState } from "react";
 
 function App() {
-  let [likeCounter, setLikeCounter] = useState(0);
+  // let list = useState(0);
+  // let [likeCounter, setLikeCounter] = list;
+
+  // let likeCounter = 0;
+  let [likeCounter, setLikeCounter] = useState(0); // ES6
 
   let likeMe = () => {
-    let newLikeCounter = likeCounter + 1;
-    setLikeCounter(newLikeCounter);
+    let newCounter = likeCounter + 1;
 
-    console.log(newLikeCounter);
+    // Asking the REACT TO UPDAT THE STATE / UI
+    setLikeCounter(newCounter);
   };
 
   return (
     <div>
       <h1>{likeCounter}</h1>
-
       <input type="button" value="Like Me" onClick={likeMe} />
     </div>
   );
