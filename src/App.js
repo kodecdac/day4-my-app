4ddcf65e015d3752460a574cb43460b35cafda72
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-03-03 10:23:46 +0530

    styling with bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-import "./App.css";
-
 function App() {
   return (
     <div>
-      <h1 style={{ color: "red" }}>Inline Styling</h1>
-      <h1 className="my-style">External Style</h1>
-
-      <p className="beautify">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae sint
-        culpa ipsum dolores dolorem obcaecati aliquam. Quo deserunt voluptate
-        explicabo, libero ipsum natus sunt ex voluptatibus saepe dolor, nisi
-        architecto.
-      </p>
+      <h1 className="bg-success text-light">Hello World</h1>
     </div>
   );
 }
