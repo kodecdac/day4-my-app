2a35d5ae8d6345e98c0826dcfcfea6f8a8e81541
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-03-03 10:34:25 +0530

    handling evernt.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 function App() {
+  let counter = 10;
+  let title = "State Management ###";
+
+  // USED FOR EVENT HANDLING
+  let clickMe = () => {
+    console.log("Function is called!!");
+    alert();
+  };
+
   return (
     <div>
-      <h1 className="bg-success text-light">Hello World</h1>
+      <h1>{title}</h1>
+      <h1>{counter}</h1>
+
+      <input type="button" value="Click Me" onClick={clickMe} />
     </div>
   );
 }
