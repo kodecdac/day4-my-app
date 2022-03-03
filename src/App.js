48a58b4ea6987c28c2959cbd8b4d79ca2593a78e
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-03-03 11:37:58 +0530

    stateul list.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import { useState } from "react";
 
 export default function App() {
-  // let user = { usenrame: "rohit", follower: 100 };
-  let [user, setUser] = useState({ username: "rohit", follower: 100 });
+  let [cityList, setCityList] = useState([]);
 
-  const updateFollower = () => {
-    let newFollower = user.follower + 10;
-    setUser({ ...user, follower: newFollower });
-  };
-
-  const updateUsername = () => {
-    let newUsername = user.username + " sharma";
-    setUser({ ...user, username: newUsername });
+  let addNewCity = () => {
+    setCityList(["PUNE", ...cityList]);
   };
 
   return (
     <div>
-      <h1>
-        {user.username} has {user.follower} follower
-      </h1>
+      <h1>Lets Tweet</h1>
+      <input type="button" value="Add new Tweet" onClick={addNewCity} />
 
-      <input type="button" value="Update Follower" onClick={updateFollower} />
-      <input type="button" value="Update Username" onClick={updateUsername} />
+      {cityList.map((item) => (
+        <div>{item}</div>
+      ))}
     </div>
   );
 }
