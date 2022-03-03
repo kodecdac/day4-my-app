bc6894f868dcbb78e0b97df2658478a443237df3
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-03-03 10:06:20 +0530

    styleings fundamental

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ function App() {
   let id = 1;
   let title = "React App";
 
+  let style1 = { backgroundColor: "green", color: "white" };
+
   return (
     <div>
-      <h1>Hello World</h1>
-      <h2> {title} </h2>
+      {/** We are writing css; USING JS!!  */}
+      <h1 style={{ color: "red", background: "yellow" }}>Hello World</h1>
 
-      <p id="id1"></p>
-      <p id={id}></p>
+      {/** We follow here camelCase convention */}
+      <h2 style={{ backgroundColor: "royalblue", color: "white" }}>
+        Hello Universe!!
+      </h2>
+
+      <h2 style={style1}>Lorem ipsum dolor sit amet.</h2>
     </div>
   );
 }
